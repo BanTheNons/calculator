c801f8afa46cecdfb875e4e0ca634769ae126000
BuildTools <59115290+BanTheNons@users.noreply.github.com>
2021-06-06 18:54:14 +0300

    A few improvements along with a delete button

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,189 @@
 import { verifyKey } from "discord-interactions";
 import { PUBLIC_KEY } from "./constants";
 
+
+const components = [
+    {
+        type: 1,
+        components: [
+            {
+                type: 2,
+                custom_id: 'ac',
+                label: 'AC',
+                style: 4
+            },
+            {
+                type: 2,
+                custom_id: '(',
+                label: '(',
+                style: 1
+            },
+            {
+                type: 2,
+                custom_id: ')',
+                label: ')',
+                style: 1
+            },
+            {
+                type: 2,
+                custom_id: '^',
+                label: '^',
+                style: 1
+            },
+            {
+                type: 2,
+                custom_id: 'delete',
+                label: 'Delete',
+                style: 4
+            }
+        ]
+    },
+    {
+        type: 1,
+        components: [
+            {
+                type: 2,
+                custom_id: '7',
+                label: '7',
+                style: 2
+            },
+            {
+                type: 2,
+                custom_id: '8',
+                label: '8',
+                style: 2
+            },
+            {
+                type: 2,
+                custom_id: '9',
+                label: '9',
+                style: 2
+            },
+            {
+                type: 2,
+                custom_id: '/',
+                label: '/',
+                style: 1
+            },
+            {
+                type: 2,
+                custom_id: ' ',
+                label: ' ',
+                style: 2,
+                disabled: true
+            }
+        ]
+    },
+    {
+        type: 1,
+        components: [
+            {
+                type: 2,
+                custom_id: '4',
+                label: '4',
+                style: 2
+            },
+            {
+                type: 2,
+                custom_id: '5',
+                label: '5',
+                style: 2
+            },
+            {
+                type: 2,
+                custom_id: '6',
+                label: '6',
+                style: 2
+            },
+            {
+                type: 2,
+                custom_id: 'x',
+                label: 'X',
+                style: 1
+            },
+            {
+                type: 2,
+                custom_id: ' ',
+                label: ' ',
+                style: 2,
+                disabled: true
+            }
+        ]
+    },
+    {
+        type: 1,
+        components: [
+            {
+                type: 2,
+                custom_id: '1',
+                label: '1',
+                style: 2
+            },
+            {
+                type: 2,
+                custom_id: '2',
+                label: '2',
+                style: 2
+            },
+            {
+                type: 2,
+                custom_id: '3',
+                label: '3',
+                style: 2
+            },
+            {
+                type: 2,
+                custom_id: '-',
+                label: '-',
+                style: 1
+            },
+            {
+                type: 2,
+                custom_id: ' ',
+                label: ' ',
+                style: 2,
+                disabled: true
+            }
+        ]
+    },
+    {
+        type: 1,
+        components: [
+            {
+                type: 2,
+                custom_id: '.',
+                label: '.',
+                style: 1
+            },
+            {
+                type: 2,
+                custom_id: '0',
+                label: '0',
+                style: 2
+            },
+            {
+                type: 2,
+                custom_id: '=',
+                label: '=',
+                style: 3
+            },
+            {
+                type: 2,
+                custom_id: '+',
+                label: '+',
+                style: 1
+            },
+            {
+                type: 2,
+                custom_id: ' ',
+                label: ' ',
+                style: 2,
+                disabled: true
+            }
+        ]
+    }
+]
+
 export async function handleRequest(request: Request): Promise<Response> {
     if (!request.headers.get('X-Signature-Ed25519') || !request.headers.get('X-Signature-Timestamp')) return Response.redirect('https://advaith.io')
     const valid = verifyKey(await request.clone().arrayBuffer(), request.headers.get('X-Signature-Ed25519')!, request.headers.get('X-Signature-Timestamp')!, PUBLIC_KEY)
@@ -17,153 +200,7 @@ export async function handleRequest(request: Request): Promise<Response> {
         type: 4,
         data: {
             content: '```\n \n```',
-            components: [
-                {
-                    type: 1,
-                    components: [
-                        {
-                            type: 2,
-                            custom_id: 'ac',
-                            label: 'AC',
-                            style: 4
-                        },
-                        {
-                            type: 2,
-                            custom_id: '(',
-                            label: '(',
-                            style: 1
-                        },
-                        {
-                            type: 2,
-                            custom_id: ')',
-                            label: ')',
-                            style: 1
-                        },
-                        {
-                            type: 2,
-                            custom_id: '^',
-                            label: '^',
-                            style: 1
-                        }
-                    ]
-                },
-                {
-                    type: 1,
-                    components: [
-                        {
-                            type: 2,
-                            custom_id: '7',
-                            label: '7',
-                            style: 2
-                        },
-                        {
-                            type: 2,
-                            custom_id: '8',
-                            label: '8',
-                            style: 2
-                        },
-                        {
-                            type: 2,
-                            custom_id: '9',
-                            label: '9',
-                            style: 2
-                        },
-                        {
-                            type: 2,
-                            custom_id: '/',
-                            label: '/',
-                            style: 1
-                        }
-                    ]
-                },
-                {
-                    type: 1,
-                    components: [
-                        {
-                            type: 2,
-                            custom_id: '4',
-                            label: '4',
-                            style: 2
-                        },
-                        {
-                            type: 2,
-                            custom_id: '5',
-                            label: '5',
-                            style: 2
-                        },
-                        {
-                            type: 2,
-                            custom_id: '6',
-                            label: '6',
-                            style: 2
-                        },
-                        {
-                            type: 2,
-                            custom_id: 'x',
-                            label: 'X',
-                            style: 1
-                        }
-                    ]
-                },
-                {
-                    type: 1,
-                    components: [
-                        {
-                            type: 2,
-                            custom_id: '1',
-                            label: '1',
-                            style: 2
-                        },
-                        {
-                            type: 2,
-                            custom_id: '2',
-                            label: '2',
-                            style: 2
-                        },
-                        {
-                            type: 2,
-                            custom_id: '3',
-                            label: '3',
-                            style: 2
-                        },
-                        {
-                            type: 2,
-                            custom_id: '-',
-                            label: '-',
-                            style: 1
-                        }
-                    ]
-                },
-                {
-                    type: 1,
-                    components: [
-                        {
-                            type: 2,
-                            custom_id: '.',
-                            label: '.',
-                            style: 1
-                        },
-                        {
-                            type: 2,
-                            custom_id: '0',
-                            label: '0',
-                            style: 2
-                        },
-                        {
-                            type: 2,
-                            custom_id: '=',
-                            label: '=',
-                            style: 3
-                        },
-                        {
-                            type: 2,
-                            custom_id: '+',
-                            label: '+',
-                            style: 1
-                        }
-                    ]
-                }
-            ]
+            components
         }
     })
 
@@ -182,9 +219,17 @@ export async function handleRequest(request: Request): Promise<Response> {
             row.components.some((button: any) => button.custom_id === interaction.data.custom_id) ? clickedButton = row.components.find((button: any) => button.custom_id === interaction.data.custom_id) : undefined
         })
 
-        let equation = interaction.message.content.replace(/```/g, '').replace(/\n/g, '').replace(/.*= /, '').replace(/ /g, '')
+        let equation = interaction.message.content
+            .replace(/```/g, '')
+            .replace(/\n/g, '')
+            .replace(/.*= /, '')
+            .replace(/ /g, '')
 
-        if (equation.includes('Error')) equation = ''
+        if (equation.includes('Error')) equation = equation
+            .match(/\(.+\)/)[0]
+            .replace('(', '')
+            .replace(')', '')
+        || ''
 
         if (clickedButton.style === 1) return respond({
             type: 7,
@@ -204,35 +249,44 @@ export async function handleRequest(request: Request): Promise<Response> {
 
             const { evaluate } = await import('mathjs')
 
-            const equation = interaction.message.content
-                .replace(/```/g, '')
-                .replace(/\n/g, '')
-                .replace(/x/g, '*')
-
             try {
-                const result = evaluate(equation) || ' '
+                const result = evaluate(equation.replace(/x/g, '*')) || 0
 
                 return respond({
                     type: 7,
                     data: {
-                        content: `\`\`\`\n${equation.replace(/\*/g, 'x')} = ${result}\n\`\`\``
+                        content: `\`\`\`\n${equation} = ${result}\n\`\`\``
                     }
                 })
             } catch (e) {
                 return respond({
                     type: 7,
                     data: {
-                        content: '```Error: Invalid Equation```'
+                        content: `\`\`\`Error: Invalid Equation${equation ? ` (${equation})` : ''}\`\`\``
                     }
                 })
             }
 
         }
 
-        return respond({
+        if (interaction.data.custom_id === 'ac') return respond({
             type: 7,
             data: {
                 content: '```\n \n```'
+            }
+        })
+
+        if (interaction.data.custom_id === 'delete') return respond({
+            type: 7,
+            data: {
+                content: `\`\`\`\n${equation.slice(0, -1) || ' '}\n\`\`\``
+            }
+        })
+
+        return respond({
+            type: 7,
+            data: {
+                components
             }
         })
 
